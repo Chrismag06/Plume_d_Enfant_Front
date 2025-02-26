09ee081649857a9ac0bcac6d90fd853f95d17f7f
chrismag06 <chrismag06000@gmail.com>
2025-02-26 17:07:46 +0100

    text supp

--- src/app/service/tts.service.ts
+++ src/app/service/tts.service.ts
@@ -7,15 +7,14 @@ import { Injectable } from '@angular/core';
 export class TtsService {
 
   private apiUrl = 'http://localhost:8080/tts/speak';
-  private apiKey = 'TON_API_KEY'; // Remplace par ta clé API
-
+ 
   constructor(private http: HttpClient) {}
 
   getSpeech(text: string, ssml: boolean = false) {
     const params = { text, ssml: ssml.toString() };
     return this.http.get(this.apiUrl, { 
       params, 
-      responseType: 'blob'  // On récupère un fichier audio (MP3)
+      responseType: 'blob'  // On récupère un fichier audio 
     });
   }
 
